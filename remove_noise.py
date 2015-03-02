665f5e9f2725fcfd373f6526d911762921d7f467
akorosov <korosov@gmail.com>
2015-03-02 10:45:01 +0100

    Create Sentinel1Image class to implement thermal noise correction

--- remove_noise.py
+++ remove_noise.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
 from nansat import *
+from sentinel1image import Sentinel1Image
 
 # remove thermal noise and anglura dependence from sigma0 in HH and HV of
 # several scence
@@ -25,62 +26,30 @@ idir = '/files/sentinel1a/'
 s1afiles = sorted(glob.glob(idir + '*.SAFE'))
 odir = '/files/sentinel1a/'
 
-clims = {'HH': [], 'HV': []}
+# remove thermal noise and anglura dependence from sigma0 in HH and HV of
+# several scence
+
+# min/max of sigma0 for non-corr and corrected values
+vmax = {'HH' : [-5, -5], 'HV' : [-18, -17]}
+vmin = {'HH' : [-25, -12], 'HV' : [-33, -23]}
+
+
+resizeFactor = 0.1
+
+idir = '/files/sentinel1a/'
+s1afiles = sorted(glob.glob(idir + '*.SAFE'))
+odir = '/files/sentinel1a/'
 
 for s1afile in s1afiles:
     print s1afile
     ofile = os.path.join(odir, os.path.splitext(os.path.split(s1afile)[1])[0])
-    n = Nansat(s1afile)
+    n = Sentinel1Image(s1afile)
     print 'open - OK'
     n.resize(resizeFactor, eResampleAlg=0) # nearest neighbour, just for test
-    # load data from file
-    eaMatrix = n['elevation_angle']
 
     for pol in ['HH', 'HV']:
-        sigma0 = n['sigma0_%s' % pol]
-        f = Figure(10 * np.log10(sigma0))
-        cmin, cmax = f.clim_from_histogram(ratio=0.95)
-        f.process(cmin=vmin[pol][0], cmax=vmax[pol][0], cmapName='gray')
-        f.save(ofile + 'sigma0_%s.jpg' % pol)
-
-        # load noise from file
-        s1aNoise = np.load('s1a_%s_thermal_noise.npz' % pol)
-        noiseArrays = [s1aNoise[noiseFile] for noiseFile in sorted(s1aNoise.files)]
-
-        # find minimum noise
-        maxNoise = 0
-        for noiseArray in noiseArrays:
-            maxNoise = max(maxNoise, max(noiseArray[1]))
-            print maxNoise
-
-        # subtract maximum noise from other noise values
-        for noiseArray in noiseArrays:
-            noiseArray[1] -= maxNoise
-
-
-        noiseMatrix = np.zeros_like(eaMatrix) + np.nan
-        for noiseArray in noiseArrays:
-            minEA = np.nanmin(noiseArray[0])
-            maxEA = np.nanmax(noiseArray[0])
-            print minEA, maxEA
-
-            # train interpolator
-            fc = sp.InterpolatedUnivariateSpline(noiseArray[0], noiseArray[1], k=3)
-
-            # calculate noise
-            swathMask = (eaMatrix >= minEA) * (eaMatrix <= maxEA)
-            noiseMatrix[swathMask] = fc(eaMatrix[swathMask])
-
-        del swathMask
-
-        sigma0 = sigma0 - noiseMatrix
-        del noiseMatrix
-
-        f = Figure(10 * np.log10(sigma0))
-        f.process(cmin=vmin[pol][1], cmax=vmax[pol][1], cmapName='gray')
-        f.save(ofile + 'sigma0_%scor.jpg' % pol)
-        del sigma0
-
-
-    del eaMatrix
+        n.write_figure(ofile + 'sigma0_%scor.jpg' % pol,
+                       'sigma0_%s' % pol,
+                       clim='hist', cmapName='gray')
+        raise
     del n
---
+++ sentinel1image.py
@@ -0,0 +1,62 @@
+import os
+import inspect
+
+import numpy as np
+import scipy.interpolate as sp
+
+from nansat import Nansat
+
+class Sentinel1Image(Nansat):
+    ''' Noise removal, ice/water classification '''
+
+    def __getitem__(self, bandID):
+        ''' Returns sigma0 corrected for thermal noise '''
+        # get band, name and data
+        band = self.get_GDALRasterBand(bandID)
+        name = band.GetMetadata().get('name', '')
+        dataArray = Nansat.__getitem__(self, bandID)
+
+        # return original data from other bands
+        if name not in ['sigma0_HH', 'sigma0_HV']:
+            return dataArray
+
+        # get polarization
+        pol = name[-2:]
+
+        # load presaved thermal noise estimates
+        selfDir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        s1aNoise = np.load(os.path.join(selfDir, 's1a_%s_thermal_noise.npz' % pol))
+        noiseArrays = [s1aNoise[noiseFile] for noiseFile in sorted(s1aNoise.files)]
+
+        # find maximum noise
+        maxNoise = 0
+        for noiseArray in noiseArrays:
+            maxNoise = max(maxNoise, max(noiseArray[1]))
+
+        # subtract maximum noise from other noise values
+        for noiseArray in noiseArrays:
+            noiseArray[1] -= maxNoise
+
+        # get elevation angle and original sigma0 from Nansat
+        eaMatrix = self['elevation_angle']
+
+        # create noise matrix and fill with noise for eah swath
+        for noiseArray in noiseArrays:
+            minEA = np.nanmin(noiseArray[0])
+            maxEA = np.nanmax(noiseArray[0])
+
+            # train interpolator
+            fc = sp.InterpolatedUnivariateSpline(noiseArray[0], noiseArray[1], k=3)
+
+            # calculate noise
+            swathMask = (eaMatrix >= minEA) * (eaMatrix <= maxEA)
+            noiseValues = fc(eaMatrix[swathMask])
+
+            dataArray[swathMask] = dataArray[swathMask] - noiseValues
+            # free mem
+            del noiseValues
+            del swathMask
+        del eaMatrix
+
+        return dataArray
+
