df4b5de3b1fcfbb61bd37d2a79301de4193a5ecc
akorosov <korosov@gmail.com>
2016-08-03 10:12:55 +0200

    '#2 switch to new noise correction'

--- 01calculate_text_features.py
+++ 01calculate_text_features.py
@@ -6,11 +6,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mahotas
 
-from sentinel1image import Sentinel1Image
+from sentinel1denoised.S1_EW_GRD_NoiseCorrection import Sentinel1Image
 from sar2ice import convert2gray, get_texture_features
 
 # find input files
-idir = '/files/sentinel1a/'
+idir = '/files/sentinel1a/safefiles/'
 odir = '/files/sentinel1a/odata/'
 ifiles = sorted(glob.glob(idir + '*.SAFE'))
 
@@ -21,6 +21,12 @@ l   = 64
 sigma0_max = {'HH': 0,   'HV': -14}
 sigma0_min = {'HH': -15, 'HV': -26}
 threads = 6
+r1,r2 = 0,None # no crop
+c1,c2 = 0,None # no crop
+
+# crop using the following limits:
+r1,r2 = 1000,5000
+c1,c2 = 1000,5000
 
 for ifilepath in ifiles:
     ifile = os.path.split(ifilepath)[1]
@@ -39,6 +45,14 @@ for ifilepath in ifiles:
         #s1i.crop(2000, 2000, 1000, 1000)  # for testing only
         print 'Read sigma0_%s from %s' % (pol, ifile)
         sigma0 = s1i['sigma0_%s' % pol]
+        print 'Create watermask'
+        wm = s1i.watermask()[1]
+        s1i = None
+        del s1i
+
+        # crop
+        sigma0 = sigma0[r1:r2, c1:c2]
+        wm = wm[r1:r2, c1:c2]
 
         # make full res JPG
         plt.imsave(ofile + 'sigma0.jpg', sigma0, vmin=sigma0_min[pol], vmax=sigma0_max[pol], cmap='gray')
@@ -48,7 +62,6 @@ for ifilepath in ifiles:
 
         # mask land
         print 'mask land'
-        wm = s1i.watermask()[1]
         sigma0[wm == 2] = 0
 
         # get texture features
