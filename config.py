41fa719d1f73e277a9ff96155b2fee8156083045
jeong1park <orepaku.android@gmail.com>
2017-02-10 14:48:31 +0100

    introduced "config" file to control processing details

---
+++ config.py
@@ -0,0 +1,63 @@
+import os
+
+def get_env():
+
+    env = {}
+    env['inputDirectory'] = '/Volumes/ExFAT2TB/Sentinel1A/FramStrait/'
+    env['outputDirectory'] = '/Volumes/ExFAT2TB/Sentinel1A/odata_FramStrait/'
+    env['textureFeatureNormalizationFilePrefix'] = 'norm01'
+    env['myZonesSuffix'] = '_my_zones.png'
+    env['supportVectorMachineFile'] = 'svm.pickle'
+    env['multiLookFactor'] = 1
+    env['stepSize'] = 25
+    env['subwindowSize'] = 25
+    env['grayLevel'] = 64
+    env['sigma0_max'] = {'HH':  -1.75, 'HV': -14.75}
+    env['sigma0_min'] = {'HH': -24.75, 'HV': -30.25}
+    env['numberOfThreads'] = 4
+    env['textureFeatureAlgorithm'] = 'averagedGLCM'
+    env['skewnessThreshold'] = 0
+    env['textureFeatureNormalization'] = 'boxcox'
+    env['textureFeatureID'] = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+    env['numberOfPrincialComponent'] = 6
+    env['numberOfKmeansCluster'] = 15
+    env['princialComponentID'] = [0,1,2,3,4,5]
+    env['zoneColors'] = [100,200]
+
+    if not os.path.exists(env['inputDirectory']):
+            raise IOError('cannot find input directory %s' % env['inputDirectory'])
+    
+    if not os.path.exists(env['outputDirectory']):
+            raise IOError('cannot find output directory %s' % env['outputDirectory'])
+
+    if env['multiLookFactor']%1!=0 or env['multiLookFactor']<=0:
+        raise ValueError('"multiLookFactor" must be positive integer.')
+    
+    if env['stepSize']%1!=0 or env['stepSize']<=0:
+        raise ValueError('"stepSize" must be positive integer.')
+    
+    if env['subwindowSize']%1!=0 or env['subwindowSize']<=0:
+        raise ValueError('"subwindowSize" must be positive integer.')
+    
+    if env['grayLevel']%1!=0 or env['grayLevel']<=0:
+        raise ValueError('"grayLevel" must be positive integer.')
+    
+    if env['numberOfThreads']%1!=0 or env['numberOfThreads']<=0:
+        raise ValueError('"numberOfThreads" must be positive integer.')
+    
+    if env['textureFeatureAlgorithm'] not in ['averagedGLCM','averagedTFs']:
+        raise KeyError('"textureFeatureAlgorithm" must be "averagedGLCM" or "averagedTFs".')
+    
+    if env['textureFeatureNormalization'] not in ['log','boxcox']:
+        raise KeyError('"textureFeatureNormalization" must be "log" or "boxcox".')
+
+    if ( sum([ID < 13 for ID in env['textureFeatureID'] ])
+         != len(env['textureFeatureID']) ):
+        raise KeyError('"textureFeatureID" contains wrong number.')
+
+    if ( sum([ID < env['numberOfPrincialComponent']
+              for ID in env['princialComponentID']])
+         != env['numberOfPrincialComponent'] ):
+        raise KeyError('"princialComponentID" contains wrong number.')
+
+    return env
